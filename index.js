e4feb40df6d7017c8303b8245c7bd4128bd08de1
Revan Sarbast <revansarbast8@gmail.com>
2020-05-15 23:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 function greet(time){
-  if (time < 12){
-    time += 12;
-  }
-  if(time >= 12 ){
-    return "Good Morning"
-  }
+  
 }
 function displayMessage(msg) {
   let message = document.querySelector('#greeting');
